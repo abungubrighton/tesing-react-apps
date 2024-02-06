44639277feefb4c53b3b715fd0ae58b02e5ab4db
abungubrighton <brighton@ibuqacapital.com>
2024-02-06 09:40:01 +0300

    chore:get by role name option

--- src/components/application.test.tsx
+++ src/components/application.test.tsx
@@ -1,22 +1,28 @@
-import { render, screen } from "@testing-library/react"
-import{Application} from "./Application"
-import React from "react"
-describe("Application component", () => { 
-    test("Application renders correctly", () => { 
-        render(<Application />)
-        let nameElement = screen.getByRole('textbox');
+import { render, screen } from "@testing-library/react";
+import { Application } from "./Application";
+describe("Application component", () => {
+    test("Application renders correctly", () => {
+        render(<Application />);
+        let nameElement = screen.getByRole("textbox", {
+            name: "Name",
+        });
         expect(nameElement).toBeInTheDocument();
 
+        // check that the Bio information is available
+        let bioElement = screen.getByRole("textbox", {
+            name: "Bio",
+        });
+        expect(bioElement).toBeInTheDocument();
         // check that the select element is also in the document
-        let jobSelectionElement = screen.getByRole('combobox');
+        let jobSelectionElement = screen.getByRole("combobox");
         expect(jobSelectionElement).toBeInTheDocument();
 
         // check that the terms and condition element is also in the document
-        let termsElement = screen.getByRole('checkbox');
+        let termsElement = screen.getByRole("checkbox");
         expect(termsElement).toBeInTheDocument();
 
         // check that the Submit button element is also in the document
-        let submitButtonElement = screen.getByRole('button');
+        let submitButtonElement = screen.getByRole("button");
         expect(submitButtonElement).toBeInTheDocument();
-    })
-})
+    });
+});
